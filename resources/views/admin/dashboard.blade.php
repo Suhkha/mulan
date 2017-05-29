a8b054ff84dc2e07dfcc1bd6e815a62421a12ef2
Jessica Villa <jessica.villa@nurun.mx>
2017-05-29 18:57:12 -0500

    login admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard Admin</div>
+                <div class="panel-heading">Dashboard ADMIN</div>
 
                 <div class="panel-body">
                     You are logged in!
