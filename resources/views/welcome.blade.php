3be669321463f87e8087a02beb71b0a78ce167d4
Jessica Villa <jessica.villa@nurun.mx>
2017-05-30 10:09:36 -0500

    demo for laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Demo
                 </div>
 
                 <div class="links">
