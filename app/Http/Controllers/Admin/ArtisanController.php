61ba45d0936185388a106935210b66f14fe2c9c0
Jessica Villa <jesvilla1@mxmexpww6496.global.publicisgroupe.net>
2017-08-10 13:55:00 -0500

    x test

--- app/Http/Controllers/Admin/ArtisanController.php
+++ app/Http/Controllers/Admin/ArtisanController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AdminStoreArtisan;
