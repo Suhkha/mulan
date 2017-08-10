260ef622420b233d9b1d6305ce3df7cb8846e04e
Jessica Villa <jesvilla1@mxmexpww6496.global.publicisgroupe.net>
2017-08-10 12:36:40 -0500

    test aws s3 images

--- app/Http/Controllers/Admin/ArtisanController.php
+++ app/Http/Controllers/Admin/ArtisanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AdminStoreArtisan;
